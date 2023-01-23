9327e9f4a980d4de08be6bde6edd213cbac92805
Mehdi Najdi <mehdi.najdi@yahoo.com>
2023-01-23 10:53:39 +0000

    Class update

--- src/PointOfSale/Menu.java
+++ src/PointOfSale/Menu.java
@@ -1,9 +1,84 @@
 package PointOfSale;
 
+import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Menu {
+    public static void main(String[] arg) {
+
+        List<String> order = new ArrayList<String>();
+
+        Scanner takeOrder = new Scanner(System.in);
+
+        System.out.print("Table Number? (1 to 10) ");
+        byte number = takeOrder.nextByte();
+
+        if (number < 1 || number > 10)
+            System.out.println("Table number: 1 to 10! ");
+
+        System.out.println("""
+                        Our Menu:
+                Pizza ......... £10
+                Burger ........  £6
+                Chips .........  £4
+                Ice_Cream .....  £3
+                Drinks ........  £2
+                """);
+
+        System.out.println("What would you like? ");
+
+        String item;
+        float runningTotal = 0;
+        String priceFormatted = null;
+        while (true) {
+            item = takeOrder.nextLine().toLowerCase();
+            if (item.equals("stop"))
+                break;
+
+            if (item.equals("basket"))
+                System.out.println(order);
+
+            switch (item) {
+                case "pizza":
+                    runningTotal += 9.99;
+                    break;
+                case "burger":
+                    runningTotal += 5.99;
+                    break;
+                case "chips":
+                    runningTotal += 2.99;
+                    break;
+                case "ice-cream":
+                    runningTotal += 3.50;
+                    break;
+                case "drinks":
+                    runningTotal += 2;
+                    break;
+                default:
+                    System.out.println("Please choose from the menu!");
+                    continue;
+            }
+
+//                order = addItem(order);
+//                order = removeItem(order);
+//                cancelOrder(order);
+//                runningTotal = calculateDiscount(runningTotal);
+
+            priceFormatted = NumberFormat.getCurrencyInstance().format(runningTotal);
+
+            order.add(item.trim());
+
+            System.out.println("Item Added.");
+            System.out.println("select more or type \"basket\" to view your basket! ");
+            System.out.println("Or type \"stop\" to place the order! ");
+        }
+        System.out.println("Table number " + number);
+        System.out.println(order);
+        System.out.println(priceFormatted);
+    }
     private static float calculateDiscount(float runningTotal) {
         return (float) (runningTotal * 0.9);
     }
