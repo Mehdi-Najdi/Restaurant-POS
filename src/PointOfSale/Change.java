5bae9a2407b3f2c85fe8080ea8fd193bbcad9ca7
Mehdi Najdi <mehdi.najdi@yahoo.com>
2023-02-07 22:22:29 +0000

    Class update

--- src/PointOfSale/Change.java
+++ src/PointOfSale/Change.java
@@ -4,5 +4,5 @@ import java.util.List;
 
 public interface Change {
     void addItem(String item, List<String> order);
-    void removeItem(String item, List<String> order)
+    void removeItem(String item, List<String> order);
 }
