e45166d246103e76efe96250d59f03151b31058b
Mehdi Najdi <mehdi.najdi@yahoo.com>
2023-02-07 22:21:44 +0000

    Class update

--- src/PointOfSale/Change.java
+++ src/PointOfSale/Change.java
@@ -3,9 +3,6 @@ package PointOfSale;
 import java.util.List;
 
 public interface Change {
-    static void addItem(String item, List<String> order) {
-    }
-
-    static void removeItem(String item, List<String> order) {
-    }
+    void addItem(String item, List<String> order);
+    void removeItem(String item, List<String> order)
 }
--- src/PointOfSale/ChangeOrder.java
+++ src/PointOfSale/ChangeOrder.java
@@ -3,11 +3,11 @@ package PointOfSale;
 import java.util.List;
 
 public class ChangeOrder extends Menu implements Change {
-    public static void addItem(String item, List<String> order) {
+    public void addItem(String item, List<String> order) {
         order.add(item.trim());
     }
 
-    public static void removeItem(String item, List<String> order) {
+    public void removeItem(String item, List<String> order) {
         if (order.contains(item))
             order.remove(item);
         else
